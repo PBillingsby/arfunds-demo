03641a47913ea079889b5102968046ee894b2e8d
PBillingsby <philbillingsby@gmail.com>
2022-06-09 11:46:03 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react'
+import Arfund, { getAllContracts } from "arfunds";
 
 function App() {
+  const viewContracts = async () => {
+    try {
+      let contracts = await getAllContracts();
+      console.log(contracts)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  viewContracts()
+    .catch(console.error);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={getAllContracts}>
+        Get all contracts
+      </button>
     </div>
   );
 }
