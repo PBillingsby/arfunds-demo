277a51b4ed7d9a1e78e46a42c9416cd2fef34674
PBillingsby <philbillingsby@gmail.com>
2022-06-20 02:49:28 -0700

    change react version

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Arweave from 'arweave';
-import Arfund, { createPool, getAllContracts } from "arfunds";
+import Arfund, { createPool, read } from "arfunds";
 import PoolModal from './components/PoolModal'
 import {
   Tooltip,
@@ -16,6 +16,7 @@ import {
   Text,
   Link,
   Box,
+  Spinner,
   useDisclosure
 } from '@chakra-ui/react';
 import { QuestionOutlineIcon, ExternalLinkIcon } from '@chakra-ui/icons';
@@ -24,6 +25,7 @@ function App() {
   const poolObject = {}
   const [noWallet, setNoWallet] = useState(false)
   const [noDescription, setNoDescription] = useState(false)
+  const [loading, setLoading] = useState(false)
   const [data, setData] = useState()
   const { onOpen } = useDisclosure()
 
@@ -51,17 +53,22 @@ function App() {
       operatorInfo,
       rewards
     } = poolObject;
+
     try {
       if (validateFormInput(wallet, description)) return
-
+      setLoading(true)
       const txId = await createPool(arweave, title, description, "use_wallet", wallet, website, operatorInfo, rewards);
+      const fund = new Arfund(txId, arweave, true);
+
+      if (fund && fund.poolId) {
+        setData([poolObject, fund.poolId])
+        setLoading(false)
+        onOpen()
+      }
     } catch (error) {
-      // These 3 below calls are placeholder to show contract after "creating pool"
-      // They will need to be moved to the try block above when createPool works
-      const contracts = await getAllContracts(arweave);
-      setData(contracts)
-      onOpen()
       console.log(error)
+      setData({ error: error.message })
+      onOpen()
     }
   }
 
@@ -81,67 +88,79 @@ function App() {
 
   return (
     <>
-      <Center mt={20}>
-        <VStack px={16} pt={8}>
-          <FormControl>
-            <FormLabel fontSize='xl'>Pool Title</FormLabel>
-            <HStack>
-              <Input type='text' name='title' onChange={(e) => handleChange(e)} />
-            </HStack>
-          </FormControl>
-          <FormControl isInvalid={noDescription}>
-            <HStack pt={1.5}>
-              <FormLabel fontSize='xl'>Description</FormLabel>
-              <Tooltip
-                label={<DescriptionLabel />}
-                fontSize='md'
-              >
-                <QuestionOutlineIcon color='blue' />
-              </Tooltip>
-            </HStack>
-            <Textarea name='description' onChange={(e) => handleChange(e)} w='30rem' h='32vh' />
-            {noDescription && <FormErrorMessage>Description is required.</FormErrorMessage>}
-          </FormControl>
-        </VStack>
-        <VStack px={16} w={'xl'}>
-          <FormControl>
-            <FormLabel fontSize='xl'>Website</FormLabel>
-            <HStack>
-              <Input type='text' name='website' onChange={(e) => handleChange(e)} />
-            </HStack>
-          </FormControl>
-          <FormControl isInvalid={noWallet} id='wallet'>
-            <HStack>
-              <FormLabel fontSize='xl' pt={1.5}>Wallet</FormLabel>
-              <Tooltip
-                label={<WalletLabel />}
-              >
-                <QuestionOutlineIcon color='blue' />
-              </Tooltip>
-            </HStack>
-            <Input type='text' name='wallet' onChange={(e) => handleChange(e)} />
-            {noWallet && <FormErrorMessage>Arwallet address is required.</FormErrorMessage>}
-          </FormControl>
-          <FormControl>
-            <FormLabel fontSize='xl'>Operator Info</FormLabel>
-            <Input type='text' name='OperatorInfo' onChange={(e) => handleChange(e)} />
-          </FormControl>
-          <FormControl>
-            <FormLabel fontSize='xl'>Rewards</FormLabel>
-            <Input type='text' name='rewards' onChange={(e) => handleChange(e)} />
-          </FormControl>
-        </VStack>
+      <Center mt={20} maxH={'75vh'}>
+        {loading ?
+          <Center h={'410px'}>
+            <VStack>
+              <Spinner size='xl' color='blue.400' />
+            </VStack>
+          </Center>
+          :
+          <>
+            <VStack px={16} pt={8}>
+              <FormControl>
+                <FormLabel fontSize='xl'>Pool Title</FormLabel>
+                <HStack>
+                  <Input type='text' name='title' onChange={(e) => handleChange(e)} />
+                </HStack>
+              </FormControl>
+              <FormControl isInvalid={noDescription}>
+                <HStack pt={1.5}>
+                  <FormLabel fontSize='xl'>Description</FormLabel>
+                  <Tooltip
+                    label={<DescriptionLabel />}
+                    fontSize='md'
+                  >
+                    <QuestionOutlineIcon color='blue' />
+                  </Tooltip>
+                </HStack>
+                <Textarea name='description' onChange={(e) => handleChange(e)} w='30rem' h='32vh' />
+                {noDescription && <FormErrorMessage>Description is required.</FormErrorMessage>}
+              </FormControl>
+            </VStack>
+            <VStack px={16} w={'xl'}>
+              <FormControl>
+                <FormLabel fontSize='xl'>Website</FormLabel>
+                <HStack>
+                  <Input type='text' name='website' onChange={(e) => handleChange(e)} />
+                </HStack>
+              </FormControl>
+              <FormControl isInvalid={noWallet} id='wallet'>
+                <HStack>
+                  <FormLabel fontSize='xl' pt={1.5}>Wallet</FormLabel>
+                  <Tooltip
+                    label={<WalletLabel />}
+                  >
+                    <QuestionOutlineIcon color='blue' />
+                  </Tooltip>
+                </HStack>
+                <Input type='text' name='wallet' onChange={(e) => handleChange(e)} />
+                {noWallet && <FormErrorMessage>Arwallet address is required.</FormErrorMessage>}
+              </FormControl>
+              <FormControl>
+                <FormLabel fontSize='xl'>Operator Info</FormLabel>
+                <Input type='text' name='operatorInfo' onChange={(e) => handleChange(e)} />
+              </FormControl>
+              <FormControl>
+                <FormLabel fontSize='xl'>Rewards</FormLabel>
+                <Input type='text' name='rewards' onChange={(e) => handleChange(e)} />
+              </FormControl>
+            </VStack>
+          </>
+        }
       </Center>
       <VStack p={12}>
-        <Button mt={8} onClick={(e) => handlePoolCreate(e)} px={8} mx={16} size='auto' fontSize='3rem' bg={'#eee'} border={'1px solid black'} _hover={{ bg: '#fff' }}>Create Pool</Button>
+        <Button mt={8} onClick={(e) => handlePoolCreate(e)} px={8} mx={12} size='auto' fontSize='3rem' bg={'#eee'} border={'1px solid black'} _hover={{ bg: '#fff' }}>
+          {loading ? "Creating...." : "Create Pool"}
+        </Button>
         <Box pt={12}>
           <Text fontSize='xs'>Fill all fields to create a pool</Text>
           <Text fontSize='xs'>You can get an ArConnect wallet from <Link href='https://www.arconnect.io/' isExternal>here <ExternalLinkIcon /></Link></Text>
         </Box>
       </VStack>
       {/* This needs to be replaced with REAL value. This is currently first index of ALL contracts */}
-      {data && <PoolModal data={data[0]} arweave={arweave} />}
-      <Center position='fixed' bottom='0' left='0' w='100%'>
+      {data && <PoolModal data={data} loading={loading} />}
+      <Center bg='#eee' position='fixed' bottom='0' left='0' w='100%'>
         <footer>Heroes of History - 2022</footer>
       </Center>
     </>
--- src/components/PoolModal.jsx
+++ src/components/PoolModal.jsx
@@ -1,7 +1,6 @@
-import { useState, useEffect } from 'react'
-import Arfund, { read } from "arfunds";
+import { useEffect } from 'react'
 
-import { CopyIcon } from '@chakra-ui/icons';
+import { CopyIcon, CheckIcon } from '@chakra-ui/icons';
 
 import {
   Modal,
@@ -12,63 +11,84 @@ import {
   ModalBody,
   ModalCloseButton,
   Text,
+  VStack,
   HStack,
   Button,
+  Link,
   useDisclosure
 } from '@chakra-ui/react'
 
 const PoolModal = (props) => {
   const { isOpen, onClose, onOpen } = useDisclosure()
-  const [returnedData, setReturnedData] = useState()
 
-  useEffect(() => {
-    const { data, arweave } = props
-    const fetchData = async () => {
-      const fund = new Arfund(data, arweave, true)
-      const readData = await fund.getState()
-      onOpen()
-      setReturnedData(readData)
-    }
-    fetchData()
-      .catch(console.error);
-  }, [])
-
+  const poolObject = props.data[0]
+  const poolId = props.data[1]
   const mint = () => {
     console.log('Mint')
   }
 
+  useEffect(() => {
+    const loadModal = () => {
+      onOpen()
+    }
+    loadModal()
+  }, [])
+
+  const addressParser = (address) => {
+    return (
+      `${address.slice(0, 9)}...${address.slice(address.length - 9)}`
+    )
+  }
 
   return (
     <>
       {<Modal closeOnOverlayClick={true} isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay backdropFilter='blur(1.5px) hue-rotate(90deg)' />
-        {returnedData && (
-          <ModalContent>
-            <ModalHeader pt={4} fontWeight='normal' fontSize='2xl'>{returnedData.title}</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-              <div>
-                <HStack>
-                  <Text>
-                    <Text as={'strong'}>Contract:</Text> {`${returnedData.owner.slice(0, 9)}...${returnedData.owner.slice(returnedData.owner.length - 9)}`}
-                  </Text>
-                  <CopyIcon onClick={() => navigator.clipboard.writeText(returnedData.owner)} />
-                </HStack>
-                {returnedData.ownerInfo && <Text><Text as={'strong'}>Owner Info:</Text> {returnedData.ownerInfo}</Text>}
-                {returnedData.website && <Text><Text as={'strong'}>Website:</Text> {returnedData.website}</Text>}
-                {returnedData.useOfProceeds && <Text><Text as={'strong'}>Use of Proceeds:</Text> {returnedData.useOfProceeds}</Text>}
-                {returnedData.rewards && <Text><Text as={'strong'}>Rewards:</Text> {returnedData.rewards}</Text>}
-                {returnedData.totalContributions && <Text><Text as={'strong'}>Total Contributions:</Text> {returnedData.totalContributions}</Text>}
-                {returnedData.totalSupply && <Text><Text as={'strong'}>Total Supply:</Text> {returnedData.totalSupply}</Text>}
-              </div>
-            </ModalBody>
-            <ModalFooter margin='0 auto'>
-              <Button bg={'#eee'} border={'1px solid black'} _hover={{ bg: '#fff' }} mr={3} onClick={mint}>
+        <ModalOverlay backdropFilter='blur(1.5px) hue-rotate(50deg)' />
+        <ModalContent>
+          <ModalHeader pt={4} fontWeight='normal' fontSize='2xl' color={poolObject.error ? 'red' : ''}>{poolObject.error ? "Error" : poolObject.title}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <div>
+              {!poolId ?
+                (<div>
+                  <Text>Check that pool wallet is empty</Text>
+                  <Text pb={6}>Check that signature wallet has $AR tokens on it</Text>
+                  <Text fontSize='xs'>{poolObject.error}</Text>
+                </div>)
+                :
+                (<VStack>
+                  <HStack>
+                    {poolId && <Text><Text as={'strong'}>Pool Id:</Text> {addressParser(poolId)}</Text>}
+                    <CopyIcon onClick={() => navigator.clipboard.writeText(poolId)} />
+                  </HStack>
+                  <HStack>
+                    {poolObject.wallet && <Text><Text as={'strong'}>Contract Owner:</Text> {addressParser(poolObject.wallet)}</Text>}
+                    <CopyIcon onClick={() => navigator.clipboard.writeText(poolObject.owner)} />
+                  </HStack>
+                  {poolObject.operatorInfo && <Text><Text as={'strong'}>Owner Info:</Text> {poolObject.operatorInfo}</Text>}
+                  {poolObject.website && <Text><Text as={'strong'}>Website:</Text> <Link href={poolObject.website}>{poolObject.website}</Link></Text>}
+                  {poolObject.description && <Text><Text as={'strong'}>Use of Proceeds:</Text> {poolObject.description}</Text>}
+                  {poolObject.rewards && <Text><Text as={'strong'}>Rewards:</Text> {poolObject.rewards}</Text>}
+                  {!props.loading && (
+                    <>
+                      <CheckIcon color='green' boxSize={6} />
+                      <Text fontSize='xs'>pool created</Text>
+                    </>
+                  )
+                  }
+                </VStack>
+                )}
+            </div>
+          </ModalBody>
+          <ModalFooter margin='0 auto'>
+            {
+              !props.loading && !poolObject.error && <Button bg={'#eee'} border={'1px solid black'} _hover={{ bg: '#fff' }} mr={3} onClick={mint}>
                 Mint
               </Button>
-            </ModalFooter>
-          </ModalContent>
-        )}
+            }
+          </ModalFooter>
+        </ModalContent>
+        }
       </Modal>
       }
     </>
